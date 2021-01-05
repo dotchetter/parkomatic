28400266133533eb6f2da3b1ade9ecd45ef66b75
AntonIot19 <50829762+GarpKasparov@users.noreply.github.com>
2021-01-05 12:49:00 +0100

    Testing the accelerometer.

--- parkomatic-embedded-fw/parkomatic-embedded-fw.ino
+++ parkomatic-embedded-fw/parkomatic-embedded-fw.ino
@@ -8,3 +8,29 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+void loop() 
+{
+	static uint32_t last_iter;
+
+	accelerometer.update();
+
+	if (millis() - last_iter > INTERVAL)
+	{
+		Serial.print("Temperature: ");
+		Serial.println(accelerometer.getTemp());
+
+		Serial.print("Acceleration: ");
+		Serial.print("X axis: ");
+		Serial.println(accelerometer.getAccX());
+		
+		Serial.print("Y axis: ");
+		Serial.println(accelerometer.getAccY());
+
+		Serial.print("Z axis: ");
+		Serial.println(accelerometer.getAccZ());
+
+		last_iter = millis();
+	}
+}
