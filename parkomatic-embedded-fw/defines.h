99618628e8fac679bff8dcd36fdb1e26184ff320
Simon Olofsson <dotchetter@protonmail.ch>
2021-01-06 15:55:00 +0100

    Working sketch based on the example for the GSM module.
    Sending a request to the time server worldtimeapi.org to fetch
    current time, using GSM / GPRS connectivity.

---
+++ parkomatic-embedded-fw/defines.h
@@ -0,0 +1,20 @@
+/*
+    2021-01-06 Simon Olofsson, Anton Norell
+    Parkomatic* firmware defines file
+*/
+
+
+/* Configurations */
+
+#define SECRET_PINNUMBER             ""
+#define SECRET_GPRS_APN              ""  
+#define SECRET_GPRS_LOGIN            ""    
+#define SECRET_GPRS_PASSWORD         ""     
+
+/* Intervals and time definitions */
+
+#define SECOND                       1000UL
+#define MINUTE                       60 * SECOND
+#define INTERVAL                     1 * SECOND
+#define GSM_CONNECT_MAX_DURATION     10 * SECOND
+
--- parkomatic-embedded-fw/parkomatic-embedded-fw.ino
+++ parkomatic-embedded-fw/parkomatic-embedded-fw.ino
@@ -1,9 +1,77 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MKRGSM.h>
+#include "defines.h" 
 
+const char PINNUMBER[]     = SECRET_PINNUMBER;
+const char GPRS_APN[]      = SECRET_GPRS_APN;
+const char GPRS_LOGIN[]    = SECRET_GPRS_LOGIN;
+const char GPRS_PASSWORD[] = SECRET_GPRS_PASSWORD;
+
+GSMClient client;
+GPRS gprs;
+GSM gsmAccess;
+
+char server[] = "worldtimeapi.org";
+char path[] = "/api/timezone/Europe/Stockholm.txt";
+int port = 80; 
+
+void setup() 
+{
+	Serial.begin(9600);
+	while (!Serial) {;}
+
+	Serial.println("Starting Arduino web client.");
+
+	bool connected = false;
+
+	while (!connected) 
+	{
+		if ((gsmAccess.begin(PINNUMBER) == GSM_READY) &&
+		    (gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD) == GPRS_READY)) 
+		{
+		  connected = true;
+		} 
+		else 
+		{
+		  Serial.println("Not connected");
+		  delay(1000);
+		}
+	}
+
+	Serial.println(F("Connecting to server... "));
+
+	if (client.connect(server, port)) 
+	{
+		Serial.print(F(" success."));
+	
+		client.print("GET ");
+		client.print(path);
+		client.println(" HTTP/1.1");
+		client.print("Host: ");
+		client.println(server);
+		client.println("Connection: close");
+		client.println();
+	}
+	else 
+	{
+		Serial.println("connection failed");
+	}
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+	if (client.available())
+	{
+		char c = client.read();
+		Serial.print(c);
+	}
 
+	if (!client.available() && !client.connected()) 
+	{
+		Serial.println();
+		Serial.println("disconnecting.");
+		client.stop();
+		
+		Serial.println("done.");
+		while(1){}
+	}
 }
