74963062c379f12d414cdee9a60fba2ab2939e94
Simon Olofsson <dotchetter@protonmail.ch>
2021-01-11 13:33:31 +0100

    #12 Development started on Azure IOT hub client class

---
+++ parkomatic-embedded-fw/iothubclient_mkr.cpp
@@ -0,0 +1,16 @@
+/*
+    Arduino MKR1400 Azure IOT Hub library 
+
+    Developed by Simon Olofsson 2021-01-11
+    https://github.com/dotchetter
+
+    This library is designed to work with the Arduino MKR
+    series boards, on NB, Wifi or GSM.
+
+    MIT license
+*/
+
+class IotHubClient
+{
+
+};
