cfcd398a6c46f9bfab83f2ec09c36132b94e6866
AntonIot19 <50829762+GarpKasparov@users.noreply.github.com>
2021-01-13 13:35:00 +0100

    #13 Added Lcd setup

--- parkomatic-embedded-fw/parkomatic-embedded-fw.ino
+++ parkomatic-embedded-fw/parkomatic-embedded-fw.ino
@@ -1,6 +1,16 @@
 void setup() {
   // put your setup code here, to run once:
+#include "LiquidCrystal.h"
+#include "defines.h"
+#include "ArduinoLowPower.h"
 
+LiquidCrystal lcd(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
+
+void setup() 
+{
+	lcd.setCursor(0,0);
+	lcd.print("Parkering start:");
+	lcd.begin(LCD_COLUMNS, LCD_ROWS);
 }
 
 void loop() {
