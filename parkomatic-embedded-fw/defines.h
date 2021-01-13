69681035fc52f0e1127ef507c093ff9e57fd9bd1
AntonIot19 <50829762+GarpKasparov@users.noreply.github.com>
2021-01-13 13:19:44 +0100

    #13 Defined constants

---
+++ parkomatic-embedded-fw/defines.h
@@ -0,0 +1,19 @@
+/*
+    2021-01-06 Simon Olofsson, Anton Norell
+    Parkomatic* firmware defines file
+*/
+
+
+/* Configurations */
+
+#define LCD_RS_PIN             		 12
+#define LCD_ENABLE_PIN               11  
+#define LCD_D4_PIN            		 5    
+#define LCD_D5_PIN 			         4   
+#define LCD_D6_PIN                   3
+#define LCD_D7_PIN             		 2
+
+#define LCD_COLUMNS                  16
+#define LCD_ROWS                     2
+
+
