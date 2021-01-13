5e78bdea5491b556126b14d8619a2744deb85fd8
AntonIot19 <50829762+GarpKasparov@users.noreply.github.com>
2021-01-13 22:24:05 +0100

    Removed duplicated code

--- parkomatic-embedded-fw/parkomatic-embedded-fw.ino
+++ parkomatic-embedded-fw/parkomatic-embedded-fw.ino
@@ -1,5 +1,3 @@
-void setup() {
-  // put your setup code here, to run once:
 #include "LiquidCrystal.h"
 #include "defines.h"
 #include "ArduinoLowPower.h"
@@ -13,17 +11,9 @@ void setup()
 	lcd.begin(LCD_COLUMNS, LCD_ROWS);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 void display_time()
 {
 
-	time_t time;
-	time = strtoul("1360440555", NULL, 0);
-	ctime(&time);
-
-	uint32_t timestamp = NULL; //gps.getTIme();
-	
 	lcd.setCursor(3,1);
 	lcd.print("10:30");
 }
