28cbdfa363c75a10dd9e08b8bd94fcd22e3b73bc
Simon Olofsson <dotchetter@protonmail.ch>
2021-01-14 19:44:52 +0100

    Added defines for LCD connection and MQTT properties

--- parkomatic-embedded-fw/defines.h
+++ parkomatic-embedded-fw/defines.h
@@ -8,9 +8,25 @@
 
 
 /* Configurations */
+
+#define LCD_RS_PIN             		 12
+#define LCD_ENABLE_PIN               11  
+#define LCD_D4_PIN            		 5    
+#define LCD_D5_PIN 			         4   
+#define LCD_D6_PIN                   3
+#define LCD_D7_PIN             		 2
+
+#define LCD_COLUMNS                  16
+#define LCD_ROWS                     2
+
+#define JSON_BUFSIZE                 1024
 #define MQTT_PORT				     8883
 
 /* Intervals and time definitions */
+
+#define SECRET_BROKER                ""
+#define SECRET_DEVICE_ID             ""
+
 #define SECOND                       1000UL
 #define MINUTE                       60 * SECOND
 #define PUBLISH_INTERVAL             10 * SECOND
