1acf18b9e3a43f3c7e500731901be61435db5780
AntonIot19 <50829762+GarpKasparov@users.noreply.github.com>
2021-01-19 11:13:01 +0100

    #18 working D2C and C2D communication

--- DeviceMessageHandler/DeviceMessageHandler/DeviceMessageHandler.cs
+++ DeviceMessageHandler/DeviceMessageHandler/DeviceMessageHandler.cs
@@ -8,6 +8,8 @@ using System.Net.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using Microsoft.Azure.Devices;
+using System.Threading.Tasks;
 
 namespace DeviceMessageHandler
 {
@@ -25,19 +27,25 @@ namespace DeviceMessageHandler
     public static class DeviceMessageHandler
     {
         private static HttpClient client = new HttpClient();
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=parkomatic-iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=2kCYyb1I8dhYNLsDWB5FmgVFpG0S91e3H6E4Zp4eGIE=";
 
         [FunctionName("DeviceMessageHandler")]
-        public static void Run([IoTHubTrigger("messages/events", Connection = "ConnectionString")]EventData message, ILogger log)
+        public static void Run([IoTHubTrigger("messages/events", Connection = "ConnectionString")] EventData message, ILogger log)
         {
             log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
             DeviceMessage deviceMessage = JsonConvert.DeserializeObject<DeviceMessage>(Encoding.UTF8.GetString(message.Body.Array));
 
-            string time = DateTime.Now.ToString("HH:mm");
-            log.LogInformation($"The current time: {time}");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            SendCloudToDeviceMessageAsync(deviceMessage.deviceId).Wait();
 
         }
 
-       
+        private async static Task SendCloudToDeviceMessageAsync(string deviceId)
+        {
+            var commandMessage = new
+             Message(Encoding.ASCII.GetBytes(DateTime.Now.ToString()));
+            await serviceClient.SendAsync(deviceId, commandMessage);
+        }
     }
-
 }
