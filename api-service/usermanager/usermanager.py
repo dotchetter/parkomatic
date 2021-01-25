1f91991ac86aee4fe43108a7b97c22b8ec4d6164
Simon Olofsson <dotchetter@protonmail.ch>
2021-01-25 23:22:22 +0100

    #24 UserManager class The UserManager is designed to act as a way to interact with the CRUD of User objects.
    Upon creating, updating or deleting users, these changes are also
    reflected in the database.
    
    The UserManager performs pre-defined SQL commands to retrieve the latest
    data before performing any actions.

--- api-service/usermanager/usermanager.py
+++ api-service/usermanager/usermanager.py
@@ -1,4 +1,5 @@
-from user import User
+from usermanager.user import User
+from usermanager.password import Password
 
 
 class UserManager:
@@ -12,13 +13,13 @@ class UserManager:
     database through the provided SqlCommands
     that are provided from the user.
 
-    Users are read from the database upon
+    Users can be deserialized from an SQL database after
     instantiation and can be modified as the
     'User' object allows.
     """
 
     def __init__(self):
-        self._user_lookup = dict[str: User]
+        self._username_user_map: dict[str: str] = {}
 
     def __repr__(self):
         pass
@@ -26,12 +27,58 @@ class UserManager:
     def __str__(self):
         pass
 
-    def __add__(self, other):
-        pass
-
-    def __iadd__(self, other):
+    def __iadd__(self, other: User):
         pass
 
     def new_user(self, username: str, email: str,
-                 firstname: str, lastname: str):
-        pass
+                 firstname: str, lastname: str,
+                 password_plaintext: str) -> bool:
+        """
+        Creates a new user, if the username is vacant
+        and complies with the limitations set.
+
+        :param username:
+            Username for the user
+        :param email:
+            Email for the user
+        :param firstname:
+            Firstname of the user
+        :param lastname:
+            Lastname of the user
+        :param password_plaintext:
+            Plain text password for the user
+        :returns:
+            bool, if user was created successfully
+        :raises:
+            UsernameIsTakenException: username already exists in the
+            database
+        """
+        if not self._username_available(username):
+            return False
+
+        new_user = User(username, email, firstname, lastname)
+        new_user.password = Password(password_plaintext)
+        # TODO: Insert user to database
+        return True
+
+    def delete_user(self, username: str) -> bool:
+        """
+        Deletes a user if available.
+        :param username: username of user to delete, str
+        :returns: bool, successful removal
+        """
+        try:
+            pass
+            # TODO: Delete user from database
+        except KeyError:
+            return False
+        return True
+
+    def _username_available(self, username: str) -> bool:
+        """
+        Checks whether a given username is available
+        for claim.
+        :param username: username to check, str
+        :returns: bool, available or not
+        """
+        return not bool(self._username_user_map.get(username))
