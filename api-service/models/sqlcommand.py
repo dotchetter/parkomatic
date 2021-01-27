504a78056228273902eeaa0d61e4dfc77b8e27a0
Simon Olofsson <dotchetter@protonmail.ch>
2021-01-27 21:44:55 +0100

    Moved SqlCommand file to /models/

--- api-service/models/sqlcommand.py
+++ api-service/models/sqlcommand.py
@@ -259,8 +259,10 @@ if __name__ == "__main__":
     get_device_id.where = {"device_id": "device_id_here"}
 
     insert_message = SqlCommand()
-    insert_message.insert_into = "table_name"
-    insert_message.columns = ("lon", "lat", "device_id", "epochtime")
-    insert_message.values = ("59.0000000", "18.0000000", get_device_id, 1611237337)
+    insert_message.insert_into = "users"
+    insert_message.columns = (user.columns)
+    insert_message.values = (user.values)
 
-    print(insert_message)
+    for i in dir(insert_message):
+        if getattr(insert_message, i):
+            print(i, getattr(insert_message, i))
