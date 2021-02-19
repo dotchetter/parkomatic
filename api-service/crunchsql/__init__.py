f08763f7e911e229f91a226002dc1c36a1b38b37
Simon Olofsson <dotchetter@protonmail.ch>
2021-02-19 17:27:47 +0100

    the CrunchSql module is a python package which aims to provide a ORM and easily implemented Sql queries in OOP software stacks.

---
+++ api-service/crunchsql/__init__.py
@@ -0,0 +1,4 @@
+from crunchsql.sqlcommand import SqlCommand, SqlCondition
+from crunchsql.sqlquery import SqlQuery
+from crunchsql.sqlproperty import SqlProperty
+from crunchsql.sqlserializable import SqlSerializable
