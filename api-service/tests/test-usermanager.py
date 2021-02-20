e108a73f0af08e1c5093cefba78dc2fb6c8e3661
Simon Olofsson <dotchetter@protonmail.ch>
2021-02-20 16:41:25 +0100

    Development of new unit tests

--- api-service/tests/test-usermanager.py
+++ api-service/tests/test-usermanager.py
@@ -1,5 +1,5 @@
 import unittest
-from usermanager import UserManager
+from dotenv import load_dotenv
 
 from crunchsql import SqlCondition
 from models import User, Device
@@ -11,30 +11,82 @@ from queries import DeviceEnroller,    \
                     UserRegistrator,   \
                     DeviceRegistrator
 
-class TestUserManager(unittest.TestCase):
 
-    def test_create_user(self):
-        mock_usermanager = UserManager()
+class TestQueries(unittest.TestCase):
 
-        r = mock_usermanager.new_user(username="username",
-                                      email="foo@bar.com",
-                                      firstname="firstname",
-                                      lastname="lastname",
-                                      password_plaintext="password")
+    device_finder = DeviceFinder()
+    user_finder = UserFinder()
+    device_enroller = DeviceEnroller()
+    device_disenroller = DeviceDisenroller()
 
-        self.assertTrue(r)
+    mock_user = next(user_finder(where=SqlCondition(username="dev@parkomatic.com")))
+    mock_device = next(device_finder(where=SqlCondition(device_id="devtest")))
 
-    def test_delete_user(self):
-        mock_usermanager = UserManager()
-        mock_usermanager.new_user(username="username",
-                                  email="foo@bar.com",
-                                  firstname="firstname",
-                                  lastname="lastname",
-                                  password_plaintext="password")
+    def test_devicefinder(self):
+        for i in TestQueries.device_finder():
+            self.assertIsInstance(i, Device)
 
-        r = mock_usermanager.delete_user("username")
-        self.assertTrue(r)
+    def test_userfinder(self):
+        for i in TestQueries.user_finder():
+            self.assertIsInstance(i, User)
+
+    def test_device_enroller(self):
+        self.assertTrue(TestQueries.device_disenroller(TestQueries.mock_user))
+        self.assertTrue(TestQueries.device_enroller(TestQueries.mock_user))
+
+load_dotenv()
+
+user_registrator = UserRegistrator()
+device_registrator = DeviceRegistrator()
+device_registrator(Device(device_id="devtest"))
+user = User(username="dev@parkomatic.com",
+            password=Password("password"))
 
 
-if __name__ == '__main__':
-    unittest.main()
+# print(user_registrator(user))
+
+#unittest.main(verbosity=2)
+
+"""
+    enroller = DeviceEnroller()
+    disenroller = DeviceDisenroller()
+    
+    mock_user = User()
+    mock_device = Device(device_id="2ce2fa33-4abc-4a77-bc3e-aa50b3e2ec88")
+
+    my_user = User()
+    my_user.username = "dotchetter@protonmail.ch"
+
+    my_device = Device()
+    my_device.device_id = "55b076a9-3efc-4593-9975-25f8048fa56e"
+
+
+
+    antons_device = Device()
+    antons_device.device_id = "2ce2fa33-4abc-4a77-bc3e-aa50b3e2ec88"
+
+    user_where = SqlConditon()
+    user_where["user_id"] = my_user.user_id
+
+    delete_user_cmd = SqlCommand()
+    delete_user_cmd.delete_from = getenv("UsersTable")
+    delete_user_cmd.where = SqlConditon()
+    delete_user_cmd.where["email"] = my_user.email
+
+    print(delete_user_cmd)
+
+    update_device_owner_cmd = SqlCommand()
+    update_device_owner_cmd.update = getenv("DevicesTable")
+    update_device_owner_cmd.set = SqlConditon()
+    update_device_owner_cmd.set["user_id"] = SqlCommand(select="id",
+                                                        select_from=getenv("UsersTable"),
+                                                        where=user_where)
+    update_device_owner_cmd.where = SqlConditon()
+    update_device_owner_cmd.where["device_id"] = my_device.device_id
+
+    print(update_device_owner_cmd)
+
+    db = DbManager(getenv("SqlConnectionString"))
+    """
+
+
